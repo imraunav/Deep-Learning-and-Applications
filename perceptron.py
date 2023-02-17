e2e87786d09870630f309925f6e6357fe14eaa84
imraunav <114980947+imraunav@users.noreply.github.com>
2023-02-17 17:01:52 +0530

    Add files via upload

--- perceptron.py
+++ perceptron.py
@@ -4,35 +4,43 @@ import numpy as np
 - Maybe trying to generalize the overall structure is making this more difficult. I should just try to do one task first.
 - Starting with simple threshold logic for the activation fn and avoiding all cleverness
 - Linearly Seperable data works with this perceptron 
+- Having trouble with sigmoid activation function for learning the weights. The application of gradient descent algorithm with respect to the true labels may be confusing me.
 '''
 class perceptron:
-    def __init__(self, labels, n_features, max_epoch=1000, activation='threshold', tol= 1e-3):
+    def __init__(self, labels, n_features, max_epoch=1000, activation='sigmoid', tol= 1e-3, learning_rate=0.25):
         # np.random.seed(100)
         self.max_epoch = max_epoch
         self.labels1 = labels[0]
         self.labels2 = labels[1]
         self.w = np.random.randn(n_features)
         self.bias = np.random.randn(1)
+        # self.w = np.array([1]*n_features, dtype=np.float64)
+        # self.bias = 1.0
         self.tol = tol
         self.activation = activation
+        self.learning_rate = learning_rate
         return None
+    
+    def logictic_sigmoid(self, x):
+        return 1/(1+np.exp(-x))
 
     def activation_fn(self, activation_value):
-        if self.activation == 'threshold':
-            # threshold logic
-            if activation_value > 0:
-                return 1
-            else:
-                return -1
-        elif self.activation == 'sigmoid':
+        if self.activation == 'sigmoid':
             # sigmoid(tan hyperbolic logic)
             return np.tanh(activation_value)
+            # return self.logictic_sigmoid(activation_value)
         elif self.activation == 'linear':
             # linear logic
             return activation_value
-        else:
-            # unknown activation function
-            return None
+        
+    def grad_descent(self, true_label, signal, sample):
+        if self.activation == 'sigmoid':
+            delta = (true_label-signal)*(1-(signal**2))
+        elif self.activation == 'linear':
+            delta = (true_label-signal)
+        self.w += self.learning_rate*delta*sample
+        self.bias += self.learning_rate*delta
+        return None
 
     def fit(self, class1_data, class2_data):
         '''
@@ -46,113 +54,57 @@ class perceptron:
         '''
         err_epoch = []
         for epoch in range(self.max_epoch):
-            learning_rate = 1/(epoch+1) #constant learning rate
-            err_count = 0
-            # see all class 1 data(positive class)
+            learning_rate = self.learning_rate #constant learning rate
+            err_collect = []
+            # see all class 1 data(negetive class)
+            true_label = -1
             for sample in class1_data:
-                signal = self.predict_learn(sample)
-                if signal < 0: # below the hyperplane; the points need to be above the plane
-                    delta = 1 # for threshold logic
-                    #  delta = (1-signal)*(1-(signal**2))                
-                    self.w += learning_rate*sample*delta
-                    self.bias += learning_rate*delta
-                    err_count += 1
-            # see all class 2 data(negetive class)
+                signal = self.predict_signal(sample)
+                err_collect.append(0.5*np.square(true_label-signal))
+                self.grad_descent(true_label, signal, sample)
+            # see all class 2 data(positive class)
+            true_label = 1
             for sample in class2_data:
-                signal = self.predict_learn(sample)
-                if signal > 0: # above the hyperplane; the points need to be below the plane
-                    delta = 1 # for threshold logic
-                    # delta = (-1-signal)*(1-(signal**2))                
-                    self.w -= learning_rate*sample*delta
-                    self.bias -= learning_rate*delta
-                    err_count += 1
-            error = err_count/(class1_data.shape[0] + class2_data.shape[0])
-            err_epoch.append(error)
+                signal = self.predict_signal(sample)
+                err_collect.append(0.5*np.square(true_label-signal))
+                self.grad_descent(true_label, signal, sample)
+            avg_error = np.mean(err_collect)
+            err_epoch.append(avg_error)
             # check convergence
-            if error <= self.tol:
-                break
+            # if avg_error <= self.tol:
+            #     break
 
         return err_epoch
 
-    def predict_learn(self, input):
+    def predict_signal(self, input):
+        '''
+        This function generates the signal from the perceptron
+
+        Parameter:
+        Input - Input sample for the perceptron
+
+        Return:
+        Signal - Signal generated from perceptron after activation function
+        '''
         activation_value = np.dot(self.w, input) + self.bias
         signal = self.activation_fn(activation_value)
         return signal 
 
     def predict(self, input):
-        activation_value = np.dot(self.w, input) + self.bias
-        signal = self.activation_fn(activation_value)
-        if signal > 0:
+        '''
+        Parameter:
+        Input - Input sample for the perceptron
+
+        Return:
+        Class label of the predicted class
+        '''
+        signal = self.predict_signal(input)
+        if signal < 0:
             return self.labels1
-        elif signal <= 0:
+        elif signal >= 0:
             return self.labels2 
+    def regress(self, input):
+        return self.predict_signal(input)
 
 
-
-# class perceptron:
-#     def __init__(self, n_feature=2, activation='threshold', max_epoch = 1000):
-#         self.w = np.ones((n_feature+1,)) # augmented weight vector initialised with thereshold
-#         self.labels = (-1, 1) # will use tanh for sigmoid function as activation function
-#         self.activation = activation
-#         self.max_epoch = max_epoch
-#         return None
-
-#     def fit(self, train_data, train_label):
-#         '''
-#         This method will take the training data and return the mean squared error per epoch after training the weights of the perceptron
-        
-#         Parameters:
-#         train_data - all training data to train the perceptron on
-        
-#         Return:
-#         error per epoch
-#         '''
-#         self.w = np.random.uniform(low=train_data.min(), high=train_data.max(), size=(3,))
-#         err_epoch = []
-#         for epoch in range(self.max_iter):
-#             err_count = 0
-#             learning_rate = 1/epoch # an approch suggested in class to non-leanearize the learning-rate
-            
-#             for sample, true_label in zip(train_data, train_label):
-#                 pred_label = self.predict(sample) # predict for each sample
-#                 self.w += learning_rate*(true_label-pred_label)*sample # correct for each sample
-#                 if true_label != pred_label:
-#                     err_count += 1
-            
-#             err_epoch.append(err_count)
-            
-#             #convergence criterion
-#             if err_count == 0:
-#                 break
-#         return err_epoch
-        
-
-#     def predict(self, input):
-#         '''
-#         This method will return the predictin based on the trained weights for the given input vector
-        
-#         Parameters:
-#         input - input test datapoint for prediction
-
-#         Return:
-#         class/value predicted
-#         '''
-#         #augmenting the input vector to accomodate the bias
-#         x = np.concatenate([[1], [input]], axis=0)
-#         activation_value = np.dot(self.w, x)
-#         y_hat = self.activation_fn(activation_value) #predict a label based on activation value
-#         return y_hat
-
-#     def activation_fn(self, a):
-#         if self.activation == 'threshold':
-#             if a > 0:
-#                 return 1
-#             else:
-#                 return -1
-#         # elif self.activation == 'sigmoid':
-#         #     return np.tanh(a) 
-#         # elif self.activation == 'linear':
-#         #     return a
-#         else:
-#             raise TypeError('Invalid activation function')
 
