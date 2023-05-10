3f934cecaf5f18a53bc108049c8feb3753ffc692
imraunav <raun.ghosh@gmail.com>
2023-05-10 21:27:42 +0530

    Uploading

--- ProgrammingAssignment6/handwritting_rnn.py
+++ ProgrammingAssignment6/handwritting_rnn.py
@@ -80,23 +80,23 @@ from preprocessing import process_handwriting, data_import_handwriting, Stopping
 #     rnn.save(filepath="./ProgrammingAssignment6/models/handwriting_rnn1.h5", overwrite=True, include_optimizer=True)
 #     with open("./ProgrammingAssignment6/logs/hist_handrnn1.pkl", mode="wb") as f:
 #         pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
-# def make_rnn():
-#     features=2
-#     seq_len = 200 # fixing for the sake of sanity
-#     model = Sequential()
-#     model.add(layers.Masking(mask_value=-1, input_shape=(seq_len, features)))
-#     model.add(layers.SimpleRNN(5, input_shape=(seq_len, features)))
-#     model.add(layers.Dense(5, activation="Softmax"))
-#     return model
-def make_rnn2():
+def make_rnn():
     features=2
     seq_len = 200 # fixing for the sake of sanity
     model = Sequential()
-    model.add(layers.Masking(mask_value=-1, input_shape=(None, features)))
-    model.add(layers.SimpleRNN(10, return_sequences=True))
-    model.add(layers.SimpleRNN(5))
+    model.add(layers.Masking(mask_value=-1, input_shape=(seq_len, features)))
+    model.add(layers.SimpleRNN(5, input_shape=(seq_len, features)))
     model.add(layers.Dense(5, activation="Softmax"))
     return model
+# def make_rnn2():
+#     features=2
+#     seq_len = 200 # fixing for the sake of sanity
+#     model = Sequential()
+#     model.add(layers.Masking(mask_value=-1, input_shape=(None, features)))
+#     model.add(layers.SimpleRNN(10, return_sequences=True))
+#     model.add(layers.SimpleRNN(5))
+#     model.add(layers.Dense(5, activation="Softmax"))
+#     return model
 # def make_rnn3():
 #     features=2
 #     seq_len = 200 # fixing for the sake of sanity
@@ -110,7 +110,7 @@ def make_rnn2():
 #     return model
 def main():
     tf.random.set_seed(32)
-    n = 2 # model number
+    n = 1 # model number
     data_path = "./ProgrammingAssignment6/CS671-DLA-Assignment4-Data-2022/Handwriting_Data"
     x_train, y_train, x_test, y_test = data_import_handwriting(data_path)
     # y_train = tf.keras.utils.to_categorical(y_train)
@@ -123,26 +123,28 @@ def main():
     mask_val = -1   # number unlikely to appear in normalized data
     x_train_padded = tf.keras.utils.pad_sequences(x_train, dtype=np.float64, padding="post", value=mask_val, maxlen=200)
     x_test_padded = tf.keras.utils.pad_sequences(x_test, dtype=np.float64, padding="post", value=mask_val, maxlen=200)
-    # rnn = make_rnn()
-    rnn = make_rnn2()
+    rnn = make_rnn()
+    # rnn = make_rnn2()
+    # rnn = make_rnn3()
 
     rnn.summary()
-    adam_optimizer = optimizers.Adam(learning_rate=0.001)
+    adam_optimizer = optimizers.Adam(learning_rate=0.1)
     categorical_loss = losses.SparseCategoricalCrossentropy()
     rnn.compile(optimizer=adam_optimizer, loss=categorical_loss, metrics=["accuracy"])
     earlystopping = callbacks.EarlyStopping(monitor='loss',
                                             min_delta=0.0001,
                                             patience=10,
                                             verbose=1)
+    remotemonitor=tf.keras.callbacks.RemoteMonitor(root="http://localhost:9000")
     terminate_on_nan = callbacks.TerminateOnNaN()
     # stopping_criteria = StoppingCriteria(patience=1, min_delta=1e-4)
     log = rnn.fit(x=x_train_padded, y=y_train,  
                 verbose=2, shuffle=True,
-                callbacks=[terminate_on_nan],
+                callbacks=[terminate_on_nan, remotemonitor],
                 epochs=10_000, validation_split=0)
-    rnn.save(filepath=f"./ProgrammingAssignment6/models/handwriting_rnn{n}.h5", overwrite=True, include_optimizer=True)
-    with open(f"./ProgrammingAssignment6/logs/hist_handrnn{n}.pkl", mode="wb") as f:
-        pickle.dump(log.history, f, protocol=pickle.HIGHEST_PROTOCOL)
+    # rnn.save(filepath=f"./ProgrammingAssignment6/models/handwriting_rnn{n}.h5", overwrite=True, include_optimizer=True)
+    # with open(f"./ProgrammingAssignment6/logs/hist_handrnn{n}.pkl", mode="wb") as f:
+    #     pickle.dump(log.history, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
